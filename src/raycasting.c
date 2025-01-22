ff2551f0aec3d10362f7a6fad077a4e1ef758103
Bruno <bruno.lameirinha@gmail.com>
2025-01-21 23:03:28 -0300

    Pos+dir+plane calculations ok

--- src/raycasting.c
+++ src/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: bruno <bruno@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 21:10:41 by bruno             #+#    #+#             */
-/*   Updated: 2025/01/21 20:05:30 by bruno            ###   ########.fr       */
+/*   Updated: 2025/01/21 22:52:15 by bruno            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void    ft_init_player(t_game *game)
 {
     game->player_info.vector_pos[0] = 5;
     game->player_info.vector_pos[1] = 5;
-    game->player_info.vector_dir[0] = -1;
-    game->player_info.vector_dir[1] = 0;
+    game->player_info.vector_dir[0] = 0;
+    game->player_info.vector_dir[1] = -1;
     game->player_info.x = 5;
     game->player_info.y = 5;
     game->camera.plane[0] = 0.66;
@@ -31,22 +31,32 @@ void ft_find_rays(t_game *game)
     float   multiplier; 
     float   camere_pixel[2];
     float   ray_dir[2];
+    float   pos_dir_plane[2];
+    float   pos_dir_minus_plane[2];
 
+    pos_dir_plane[0] = 0;
+    pos_dir_plane[1]= 0 ;
     pixels = 0;
     multiplier = 0;
-    while (pixels < WINDOW_WIDTH)
+    while (pixels <= WINDOW_WIDTH)
     {
         multiplier = 2.0f * ((float)pixels / (float)WINDOW_WIDTH) -1.0f;
         camere_pixel[0] = game->camera.plane[0] * multiplier;
         camere_pixel[1] = game->camera.plane[1] * multiplier;
         ray_dir[0] = game->player_info.vector_dir[0] + camere_pixel[0];
         ray_dir[1] = game->player_info.vector_dir[1] + camere_pixel[1];
+
         printf("Multiplicador: %.2f\n", multiplier);
-        printf("Pixels: %d\n", pixels);
         printf("Camere Pixel: [%.2f, %.2f]\n", camere_pixel[0], camere_pixel[1]);
         printf("Ray Dir: [%.2f, %.2f]\n", ray_dir[0], ray_dir[1]);
         
         pixels++;
     }
+    pos_dir_plane[0] = game->player_info.vector_pos[0] + game->player_info.vector_dir[0] + game->camera.plane[0];
+    pos_dir_plane[1] = game->player_info.vector_pos[1] + game->player_info.vector_dir[1] + game->camera.plane[1];
+    printf("Pos Dir Plane: [%.2f, %.2f]\n", pos_dir_plane[0], pos_dir_plane[1]);
+    pos_dir_minus_plane[0] = (game->player_info.vector_pos[0] + game->player_info.vector_dir[0]) - game->camera.plane[0];
+    pos_dir_minus_plane[1] = (game->player_info.vector_pos[1] + game->player_info.vector_dir[1]) - game->camera.plane[1];
+    printf("Pos Dir minus Plane: [%.2f, %.2f]\n", pos_dir_minus_plane[0], pos_dir_minus_plane[1]);
     
 }
